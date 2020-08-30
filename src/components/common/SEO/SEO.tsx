7b693652096729cbc189db19045440b0c572dd23
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2020-08-30 18:31:30 -0300

    fix tsc

--- src/components/common/SEO/SEO.tsx
+++ src/components/common/SEO/SEO.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-import Thumbnail from 'assets/thumbnail/thumbnail.png'
+import Thumbnail from './assets/thumbnail.png'
 
 import {
   url,
