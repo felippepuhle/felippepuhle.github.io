156990ef34e12d026e54870bed2a65cf835c4900
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2020-09-15 11:33:42 -0300

    date

--- src/components/cover-letter/Text.tsx
+++ src/components/cover-letter/Text.tsx
@@ -4,10 +4,16 @@ import { Container } from 'components/common'
 
 import { CONTAINER_SIZE } from './constants'
 
+const TODAY = new Intl.DateTimeFormat('en-US', {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+}).format(new Date())
+
 export const Text = () => (
   <section>
     <Container maxWidth={CONTAINER_SIZE}>
-      <p>September 14, 2020.</p>
+      <p>{TODAY}.</p>
     </Container>
   </section>
 )
